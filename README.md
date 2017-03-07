653bae129e345c105d8d3e5569d920487b1628d7
Lisa Durand <lisa@superdudu.net>
2017-03-07 17:58:17 +0100

    read and compare new and old Topics from discourse

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sms-bot
 Bot will send SMS after a topic is created on "A mon tour de programmer" community
+
+What we need to do :
+- Parse discourse API Json to get all topics
+- extract author + topic title + ID
+- stocker dans une variable les ID
+- reparser l'api et verifier s'il y a un nouvel ID
+- si oui, on envoie un sms !!!!
+
+questions :
+- est-ce que l'API json nous renvoie tous les latest
+- faire une tache Cron quand on sera en prod
+
+
+SMS : Auteur du sujet + Titre + lien
+
+Idées d’outils —>
+Twillio pour les sms
--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
 var request = require("request")
-
-console.log('démarrage du bot');
+var jsonfile = require('jsonfile')
 
 var url = "http://communaute.amontourdeprogrammer.fr/latest.json"
+
+var file = 'data.json' // fichier pour conserver notre tableau d'ID
+var old_topics_id = jsonfile.readFileSync(file) // notre fichier d'ID transformé en tableau
+var new_topics_id = [] // tableau vide à chaque séquence pour récuperer les nouveaux ID des topics
 
 request({
     url: url,
@@ -10,7 +13,23 @@ request({
 }, function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
-        console.log(body) // Print the json response
-    }
-})
 
+        // on cree un tableau avec les topics depuis le JSON qu'on parse
+        var array_topics = (body["topic_list"]["topics"])
+
+        // on itere sur ce tableau et on insere l'ID dans le tableau new_topics s'il n'existe pas dans le tableau old_topics
+        array_topics.forEach(function(topic) {
+          if (!old_topics_id.includes(topic["id"])) {
+            new_topics_id.push(topic["id"]);
+          }
+        }
+        );
+    }
+
+    // on crée un tableau all_topics dans lequel on concatene les 2 tableaux d'ID
+    var all_topics = new_topics_id.concat(old_topics_id);
+    // on ecrit dans le fichier data.json le tableau de tous les sujets
+    jsonfile.writeFile(file, all_topics, function (err) {
+      console.error(err)
+    })
+})
